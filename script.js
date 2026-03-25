e902c5282f1f6f91063ed69ad38cf7eb4e572eaa
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-03-25 17:21:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	if(s1==""&&s2==""){
+		return -1
+	}
+	s1=s1.toLowerCase()
+    s2=s2.toLowerCase()
+    if(s1.includes(s2)){
+        return s1.indexOf(s2) 
+    } 
+    return -1;
 }
 
 // Please do not change the code below
